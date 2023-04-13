fb00bd827fc0950af0639acc0b9b3aa0c24e8a7d
kirtigupta2458 <72331388+kirtigupta2458@users.noreply.github.com>
2023-04-13 22:34:16 +0530

    Update addButtons.js

--- js/addButtons.js
+++ js/addButtons.js
@@ -7,10 +7,7 @@ AFRAME.registerComponent("createbuttons", {
     button1.setAttribute("class", "btn btn-danger ml-3 mr-3");
 
     // 2. Create the Bill button
-    var button2 = document.createElement("button");
-    button2.innerHTML = "ORDER SUMMARY";
-    button2.setAttribute("id", "order-summary-button");
-    button2.setAttribute("class", "btn btn-danger ml-3");
+    
 
     // 3. Append somewhere
     var buttonDiv = document.getElementById("button-div");
